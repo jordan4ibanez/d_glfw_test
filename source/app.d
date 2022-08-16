eebb3a497cbcd8760316b928a01b0dceda0ed8bb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-15 23:40:51 -0400

    Compile frag and vert shader automatically

--- source/app.d
+++ source/app.d
@@ -7,6 +7,9 @@ import window.window;
 import loader = bindbc.loader.sharedlib;
 import helper.log;
 import opengl.gl_interface;
+import std.string: toStringz;
+import std.conv: to;
+import opengl.shader_compiler;
 
 void main() {
 
@@ -19,9 +22,40 @@ void main() {
         return;
     }
 
-    while(!gameWindowShouldClose()) {
+    float[] vertices = [
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    ];
+    uint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, cast(long)vertices.length, vertices.ptr, GL_STATIC_DRAW);
+    
+    string vertexShaderCode = "
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
 
-        writeln(loadedOpenGLVersion());
+    void main()
+    {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }";
+    uint vertexShader = compileShader(vertexShaderCode, GL_VERTEX_SHADER);
+
+    string fragmentShaderCode = "
+    #version 330 core
+    out vec4 FragColor;
+
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }";
+    uint fragmentShader = compileShader(fragmentShaderCode, GL_FRAGMENT_SHADER);
+
+    writeln("INITIAL LOADED GL VERSION: ", getInitialOpenGLVersion());
+    writeln("FORWARD COMPATIBILITY VERSION: ", to!string(glGetString(GL_VERSION)));
+
+    while(!gameWindowShouldClose()) {
 
         gameClearWindow();
 
