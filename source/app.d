9baba3dd0ba3ceb43697989de9ffef461225f1f5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-15 20:53:58 -0400

    Make everything work

--- source/app.d
+++ source/app.d
@@ -1,15 +1,37 @@
 import std.stdio;
 
 import bindbc.glfw;
+import bindbc.opengl;
 import glfw_interface.glfw_interface;
 import window.window;
+import loader = bindbc.loader.sharedlib;
+import helper.log;
+import opengl.gl_interface;
 
 void main() {
 
-    gameInitializeGLFWComponents();
+
+    if (gameInitializeGLFWComponents("blah")) {
+        return;
+    }
+
+    if(gameInitializeOpenGL()) {
+        return;
+    }
+
+
+
+    
 
     while(!gameWindowShouldClose()) {
-        
+
+        writeln(loadedOpenGLVersion());
+
+        gameClearWindow();
+
+        gameSwapBuffers();
+
+        glfwPollEvents();
     }
     
 }
