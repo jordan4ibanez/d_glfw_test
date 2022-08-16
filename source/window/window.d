54b72007f8b111303fde43328abf560a9281883f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-15 20:53:50 -0400

    Add a bunch of internal window handling

--- source/window/window.d
+++ source/window/window.d
@@ -2,19 +2,55 @@ module window.window;
 
 import std.stdio;
 import bindbc.glfw;
+import bindbc.opengl;
+import helper.structures;
 
 // Starts off as a null pointer
 GLFWwindow* window;
+Vector2I size;
 
+nothrow
+static extern(C) void myframeBufferSizeCallback(GLFWwindow* theWindow, int x, int y) {
+    glViewport(0,0,x,y);
+}
 
 // Stores and gives context to the C window
 void setWindowPointer(GLFWwindow* newWindow) {
-    window = newWindow;
+    window = newWindow;    
+
+    glfwGetWindowSize(window,&size.x, &size.y);
+
+    //writeln(tst);
 
     glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, &myframeBufferSizeCallback);
 }
 
 // Internally handles interfacing to C
 bool gameWindowShouldClose() {
     return glfwWindowShouldClose(window) != 0;
+}
+
+void gameSwapBuffers() {
+    glfwSwapBuffers(window);
+}
+
+Vector2I getWindowSize() {
+    return size;
+}
+
+
+void gameClearWindow() {
+
+    RGBA color = RGBA(1.0,1.0,1.0,1.0);
+
+    glClearColor(
+        GLclampf(color.r),
+        GLclampf(color.b),
+        GLclampf(color.g),
+        GLclampf(color.a)
+    );
+
+    glClear(GL_COLOR_BUFFER_BIT);    
 }
