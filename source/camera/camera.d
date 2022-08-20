75c970db8ab904c98b847388c00c771ae72def6a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-19 23:10:42 -0400

    Do camera stuff

--- source/camera/camera.d
+++ source/camera/camera.d
@@ -1,4 +1,25 @@
 module camera.camera;
 
+import std.stdio;
+import window.window;
+import math;
+import matrix_4d;
+
 // There can only be one camera in the game, this is it
 
+immutable double FOV = toRadians(60.0);
+
+immutable double Z_NEAR = 0.00000;
+
+immutable double Z_FAR = 10_000.;
+
+
+Matrix4d projectionMatrix = Matrix4d();
+
+double aspectRatio = 0;
+
+void updateCamera() {
+    aspectRatio = getAspectRatio();
+    writeln("aspect ratio is: ", aspectRatio);
+    projectionMatrix = Matrix4d().perspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+}
