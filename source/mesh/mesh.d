975bd0e0902fd293f3e08dfe2c0f2e4298bb9c4b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-20 23:34:06 -0400

    Create a usable basic mesh structure

--- source/mesh/mesh.d
+++ source/mesh/mesh.d
@@ -1,14 +1,46 @@
 module mesh.mesh;
 
+import bindbc.opengl;
+
 struct Mesh {
     bool exists = false;
-    int vao = 0;
-    int vbo = 0;
-    int ebo = 0;
-    int vertexCount = 0;
-    int texture = 0;
+    GLuint vao = 0;
+    GLuint vbo = 0;
+    GLuint ebo = 0;
+    GLuint texture = 0;
+    GLuint vertexCount = 0;
 
-    this(int vao, int vbo, int ebo, int vertexCount, int texture){
+    this(float[] vertices) {
+        // Don't bother if not divisible by 3 (x,y,z)
+        assert(vertices.length % 3 == 0 && vertices.length >= 3);
+        this.vertexCount = cast(GLuint)(vertices.length / 3);
+
+
+        glGenVertexArrays(1, &this.vao);
+        glGenBuffers(1, &this.vbo);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(this.vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices.length * float.sizeof, vertices.ptr, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, cast(void*)0);
+        glEnableVertexAttribArray(0);
+
+        // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 1);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        glBindVertexArray(0);
+    }
+
+    void render() {
+        glBindVertexArray(this.vao);
+        glDrawArrays(GL_TRIANGLES, 0, this.vertexCount);
+    }
+    /*
+    void appendGLData(int vao, int vbo, int ebo, int vertexCount, int texture){
         this.exists = true;
         this.vao = vao;
         this.vbo = vbo;
@@ -16,4 +48,5 @@ struct Mesh {
         this.vertexCount = vertexCount;
         this.texture = texture;
     }
+    */
 }
