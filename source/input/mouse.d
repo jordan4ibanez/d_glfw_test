b44320abd017c5127c7e7b732c186b6ebb923ce3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-22 04:01:45 -0400

    Automatically integrate vector

--- source/input/mouse.d
+++ source/input/mouse.d
@@ -3,9 +3,16 @@ module input.mouse;
 import vector_2d;
 import std.stdio;
 
-Vector2d position = Vector2d(0,0);
+private Vector2d oldPosition = Vector2d(0,0);
+private Vector2d position    = Vector2d(0,0);
+private Vector2d vector      = Vector2d(0,0);
 
 void mouseCallback(Vector2d newPosition) {
     writeln("mouse is at: ", newPosition);
+    vector = Vector2d(
+        oldPosition.x - newPosition.x,
+        oldPosition.y - newPosition.y
+    );
+    writeln("the mouse vector is: ", vector);
     position = newPosition;
 }
