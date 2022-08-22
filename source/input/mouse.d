5f1e95327cb54fdbb3925a2570e0a288cd61d230
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-22 19:20:35 -0400

    Make mouse move camera rotation when locked

--- source/input/mouse.d
+++ source/input/mouse.d
@@ -1,13 +1,17 @@
 module input.mouse;
 
-import vector_2d;
 import std.stdio;
+import vector_2d;
+import vector_3d;
 
 import Window = window.window;
+import Camera = camera.camera;
 
 private Vector2d oldPosition = Vector2d(0,0);
 private Vector2d position    = Vector2d(0,0);
 private Vector2d vector      = Vector2d(0,0);
+
+private double sensitivity   = 0.001;
 
 private bool locked = false;
 
@@ -20,6 +24,14 @@ void mouseCallback(Vector2d newPosition) {
     writeln("the mouse vector is: ", vector);
     position = newPosition;
     oldPosition = newPosition;
+
+    if (locked) {
+        Camera.moveRotation(Vector3d(
+            vector.x * sensitivity,
+            vector.y * sensitivity,
+            0
+        ));
+    }
 }
 
 void lockMouse(bool newLockMode) {
