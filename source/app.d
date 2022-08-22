95d31fa86b8a201626212f4f937b0fa50152c3cb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-22 01:16:20 -0400

    Plop textures into prototyping stage of struct

--- source/app.d
+++ source/app.d
@@ -100,45 +100,6 @@ void main() {
 
     setMaxDeltaFPS(10);
 
-
-    // Test of images
-    TrueColorImage myCoolImage = loadImageFromFile("textures/debug.png").getAsTrueColorImage();
-    ubyte[] myCoolImageData = myCoolImage.imageData.bytes;
-
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);    
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, myCoolImage.width(), myCoolImage.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, myCoolImageData.ptr);
-
-        // Enable texture clamping to edge
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    // Border color is nothing
-    float[4] borderColor = [0,0,0,0];
-    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor.ptr);
-
-    // Add in nearest neighbor texture filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST/*_MIPMAP_NEAREST*/);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    // glGenerateMipmap(GL_TEXTURE_2D);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textureID); 
-
-    GLenum glErrorInfo = glGetError();
-
-    if (glErrorInfo != 0) {
-        writeln("GL ERROR: ", glErrorInfo);
-        writeln("ERROR IN TEXTURE");
-        writeln("FREEZING PROGRAM TO ALLOW DIAGNOSTICS!");
-
-        while(true) {
-                
-        }
-    }
-
     // An "alive" mesh
     // Mesh thisMesh = Mesh(vertices, indices, textureCoordinates);
     
--- source/mesh/texture.d
+++ source/mesh/texture.d
@@ -1,15 +1,58 @@
 module mesh.texture;
 
+import bindbc.opengl;
+
+
+// Unlike meshes, textures never go out of scope until the program ends
+// Automatically deletes when the program ends
+Texture[string] container;
+
 struct Texture {
     bool exists = false;
-    int textureIndex = 0;
-    int width = 0;
-    int height = 0;
+    GLuint id = 0;    
+    GLuint width = 0;
+    GLuint height = 0;
 
-    this(int textureIndex, int width, int height) {
+    this(string textureName) {
         this.exists = true;
-        this.textureIndex = textureIndex;
-        this.width = width;
-        this.height = height;
+        
+        TrueColorImage tempImageObject = loadImageFromFile(textureName).getAsTrueColorImage();
+        this.width = tempImageObject.width();
+        this.height = tempImageObject.height();
+        ubyte[] tempData = myCoolImage.imageData.bytes;
+
+        GLuint textureID;
+        glGenTextures(1, &textureID);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tempData.ptr);
+
+            // Enable texture clamping to edge
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+        // Border color is nothing
+        float[4] borderColor = [0,0,0,0];
+        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor.ptr);
+
+        // Add in nearest neighbor texture filtering
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST/*_MIPMAP_NEAREST*/);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        // glGenerateMipmap(GL_TEXTURE_2D);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textureID); 
+
+        GLenum glErrorInfo = glGetError();
+
+        if (glErrorInfo != 0) {
+            writeln("GL ERROR: ", glErrorInfo);
+            writeln("ERROR IN TEXTURE");
+            writeln("FREEZING PROGRAM TO ALLOW DIAGNOSTICS!");
+
+            while(true) {
+                    
+            }
+        }
     }
 }
