8279f469681f773b9d2ade5ec32ae96cac573665
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-22 19:39:34 -0400

    Fix translation error

--- source/camera/camera.d
+++ source/camera/camera.d
@@ -70,9 +70,9 @@ Matrix4d getObjectMatrix(Vector3d offset, Vector3d rotation, float scale) {
 void updateCameraMatrix() {
     GameShader mainShader = getShader("main");
     Matrix4d stackWorkerMatrix = getCameraMatrix()
-        .translate(-position.x, -position.y, -position.z)
         .rotate(rotation.x, 0,1,0)
-        .rotate(rotation.y, 1,0,0);
+        .rotate(rotation.y, 1,0,0)
+        .translate(-position.x, -position.y, -position.z);
     float[16] floatBuffer = stackWorkerMatrix.getFloatArray();
     glUniformMatrix4fv(mainShader.getUniform("cameraMatrix"),1, GL_FALSE, floatBuffer.ptr);
 }
@@ -106,6 +106,8 @@ void setPosition(Vector3d newCameraPosition){
 
 
 void rotationLimiter(){
+
+    writeln("rotation Y: ", rotation.y);
     // Pitch limiter
     if (rotation.x > 90) {
         rotation.x = 90;
