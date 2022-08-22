6b6e57268432dc2a08ddb29f8371e9917f7d2fc7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-22 03:55:57 -0400

    Mouse now has a home :P

--- source/input/mouse.d
+++ source/input/mouse.d
@@ -3,6 +3,9 @@ module input.mouse;
 import vector_2d;
 import std.stdio;
 
+Vector2d position = Vector2d(0,0);
+
 void mouseCallback(Vector2d newPosition) {
     writeln("mouse is at: ", newPosition);
+    position = newPosition;
 }
