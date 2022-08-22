3dcf3297dc5efcd8de46bae115c51fac5bd4d124
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-21 21:39:06 -0400

    Add indices to mesh

--- source/mesh/mesh.d
+++ source/mesh/mesh.d
@@ -8,13 +8,14 @@ struct Mesh {
 
     private bool exists = false;
 
-    GLuint vao = 0;
-    GLuint vbo = 0;
-    GLuint ebo = 0;
+    GLuint vao = 0; // Vertex array object - Main object
+    GLuint pbo = 0; // Positions vertex buffer object
+    GLuint ibo = 0; // Indices vertex buffer object
+    GLuint cbo = 0; // Colors vertex buffer object
     GLuint texture = 0;
     GLuint vertexCount = 0;
 
-    this(float[] vertices) {
+    this(float[] vertices, int[] indices) {
 
         writeln("I'M ALIVE, I'M BORN");
 
@@ -22,15 +23,18 @@ struct Mesh {
         this.exists = true;
 
         // Don't bother if not divisible by 3 (x,y,z)
-        assert(vertices.length % 3 == 0 && vertices.length >= 3);
-        this.vertexCount = cast(GLuint)(vertices.length / 3);
+        assert(indices.length % 3 == 0 && indices.length >= 3);
+        this.vertexCount = cast(GLuint)(indices.length);
 
         // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
         glGenVertexArrays(1, &this.vao);
         glBindVertexArray(this.vao);
+    
 
-        glGenBuffers(1, &this.vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
+        // Positions VBO - Colors will use a similar VBO     
+
+        glGenBuffers(1, &this.pbo);
+        glBindBuffer(GL_ARRAY_BUFFER, this.pbo);
 
         glBufferData(
             GL_ARRAY_BUFFER,                // Target object
@@ -49,6 +53,19 @@ struct Mesh {
         );
         glEnableVertexAttribArray(0);
 
+
+        // Indices VBO
+
+        glGenBuffers(1, &this.ibo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.ibo);
+
+        glBufferData(
+            GL_ELEMENT_ARRAY_BUFFER,     // Target object
+            indices.length * int.sizeof, // size (bytes)
+            indices.ptr,                 // the pointer to the data for the object
+            GL_STATIC_DRAW               // The draw mode OpenGL will use
+        );
+
         GLuint glErrorInfo = glGetError();
 
         if (glErrorInfo != 0) {
@@ -62,6 +79,7 @@ struct Mesh {
         }
 
 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
         // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
         // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
         glBindVertexArray(0);
@@ -80,9 +98,13 @@ struct Mesh {
 
         glDisableVertexAttribArray(0);
 
-        // Delete the vbo
+        // Delete the positions vbo
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glDeleteBuffers(1, &this.vbo);
+        glDeleteBuffers(1, &this.pbo);
+
+        // Delete the indices vbo
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glDeleteBuffers(1, &this.ibo);
 
         // Delete the vao
         glBindVertexArray(0);
@@ -113,7 +135,8 @@ struct Mesh {
         }
 
         glBindVertexArray(this.vao);
-        glDrawArrays(GL_TRIANGLES, 0, this.vertexCount);
+        // glDrawArrays(GL_TRIANGLES, 0, this.vertexCount);
+        glDrawElements(GL_TRIANGLES, this.vertexCount, GL_UNSIGNED_INT, cast(const(void)*)0);
     }
     /*
     void appendGLData(int vao, int vbo, int ebo, int vertexCount, int texture){
