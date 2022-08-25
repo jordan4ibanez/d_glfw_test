851e281eb936d722cb36515e9c40ecc76997db36
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-25 02:06:48 -0400

    Implement functions for static class

--- source/openal/sound_manager.d
+++ source/openal/sound_manager.d
@@ -4,6 +4,7 @@ import std.stdio;
 import bindbc.openal;
 import openal.al_interface;
 import vector_3d;
+import matrix_4d;
 
 /*
 This is the work horse of the game's OpenAL implementation.
@@ -18,6 +19,37 @@ to easily access all OpenAL related components, safely.
 private SoundBuffer[256] buffers = new SoundBuffer[256];
 private SoundSource[string] soundSources;
 private SoundListener listener;
+
+void playSoundSource(string name) {
+    if ((name in soundSources) != null && !soundSources[name].isPlaying()) {
+        soundSources[name].play();
+    }
+}
+
+void removeSoundSource(string name) {
+    if ((name in soundSources) != null) {
+        soundSources[name].stop();
+        soundSources.remove(name);
+    }
+}
+
+void setAttenuationModel(ALint model) {
+    alDistanceModel(model);
+}
+
+void updateListenerPosition(Matrix4d cameraMatrix, Vector3d cameraPosition) {
+    listener.setPosition(cameraPosition);
+    Vector3d at = Vector3d();
+    // This might need testing
+    cameraMatrix.positiveZ(at).negate();
+    Vector3d up = Vector3d();
+    //This might need testing
+    cameraMatrix.positiveY(up);
+    listener.setOrientation(at, up);
+
+}
+
+
 
 
 
